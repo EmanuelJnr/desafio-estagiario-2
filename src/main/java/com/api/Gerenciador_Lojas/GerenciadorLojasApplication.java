5c1c7d89d0f28329362b363e250407d39044c2c6
Emanuel Júnior <117325229+EmanuelJnr@users.noreply.github.com>
2024-04-30 16:07:24 -0300

    Arquivos iniciais

---
+++ src/main/java/com/api/Gerenciador_Lojas/GerenciadorLojasApplication.java
@@ -0,0 +1,13 @@
+package com.api.Gerenciador_Lojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorLojasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Gerenciador_Lojas/GerenciadorLojasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Gerenciador_Lojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorLojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
